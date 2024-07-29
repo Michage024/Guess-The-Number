aa2f8019e716a43476a0377bad3d03798c4f3796
Michael Agesse <twzfi8mq@students.codeinstitute.net>
2024-07-29 21:10:06 +0000

    second validated code

--- run.py
+++ run.py
@@ -10,11 +10,13 @@ def play_game():
     guessed = False
 
     print("\nWelcome to the Guess the Number Game!")
-    print(f"I have selected a number between 1 and 7. Guess the number I have selected and type it in. You have {max_attempts} attempts to guess the right number.")
+    print(f"I have selected a number between 1 and 7.")
+    print(f"Guess the number I have selected and type it in.")
+    print(f"You have {max_attempts} attempts to guess the right number.")
 
     while not guessed and attempts < max_attempts:
         try:
-            guess = int(input("Type in a number: "))
+            guess = int(input("Type in your number: "))
             attempts += 1
 
             if guess < number_to_guess:
@@ -23,18 +25,21 @@ def play_game():
                 print("Too high!")
             else:
                 guessed = True
-                print(f"Well done! You guessed the correct number in {attempts} attempts.")
+                print(f"Well done! You guessed the correct number in ")
+                print(f"{attempts} attempts.")
         except ValueError:
             print("Please enter a valid integer.")
 
     if not guessed:
-        print(f"Sorry, you have used all {max_attempts} attempts. The correct number was {number_to_guess}.")
+        print(f"Sorry, you have used all {max_attempts} attempts.")
+        print(f"The correct number is {number_to_guess}.")
 
 
 def main():
     while True:
         play_game()
-        play_again = input("Would you like to play again? (yes/no): ").strip().lower()
+        play_again = input("Would you like to play again?")
+        play_again = input("(yes/no):").strip().lower()
         if play_again != 'yes':
             print("Thanks for playing the Guess the Number Game!")
             break
