e318081bbe4c4dd7b2cfa3fd659c40512a9ce51c
Michael Agesse <twzfi8mq@students.codeinstitute.net>
2024-07-29 20:47:01 +0000

    first correction after validation

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 
 import random
 
+
 def play_game():
     number_to_guess = random.randint(1, 7)
     attempts = 0
@@ -10,7 +11,7 @@ def play_game():
 
     print("\nWelcome to the Guess the Number Game!")
     print(f"I have selected a number between 1 and 7. Guess the number I have selected and type it in. You have {max_attempts} attempts to guess the right number.")
-    
+
     while not guessed and attempts < max_attempts:
         try:
             guess = int(input("Type in a number: "))
@@ -25,9 +26,10 @@ def play_game():
                 print(f"Well done! You guessed the correct number in {attempts} attempts.")
         except ValueError:
             print("Please enter a valid integer.")
-    
+
     if not guessed:
         print(f"Sorry, you have used all {max_attempts} attempts. The correct number was {number_to_guess}.")
+
 
 def main():
     while True:
@@ -36,6 +38,7 @@ def main():
         if play_again != 'yes':
             print("Thanks for playing the Guess the Number Game!")
             break
+
 
 if __name__ == "__main__":
     main()
